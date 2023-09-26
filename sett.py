e9e15716d737b919b953c43464e59efd99417665
B-M <76398498+Danker-Bot@users.noreply.github.com>
2023-09-25 21:50:07 -0300

    Add files via upload

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'Ingenieria420'
+
+whatsapp_token = 'EAAC1Kww1W0ABO8ctEsOGP4kO4ReWSxxcp4MhAhgrQvTZAoYEd8tvsrGcLz6w7jRZAFmnr8lqG4DkesMGBa80b8VlwfQ2ownGZBZC4RksQ8A3ZBeuB70yr7y45aTw2i7RPZBwqqWWbwd4ZBByrSXYDMEpsIMxL4PvI67CRBm5xE37s87kpkb0SzKzxTwuK7Rha1OmZCkJUsdlDZABHNBTt'
+
+whatsapp_url = 'https://graph.facebook.com/v17.0/123575767504478/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
